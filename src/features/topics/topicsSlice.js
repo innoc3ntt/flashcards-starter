c1d028e63c55e486d770336440871c7c26a7e169
Tim <timothy.tan.93@gmail.com>
2021-07-08 12:08:03 +0800

    at step 14, currently error reading

--- src/features/topics/topicsSlice.js
+++ src/features/topics/topicsSlice.js
@@ -13,12 +13,15 @@ export const topicsSlice = createSlice({
         quizIds: []
       };
     },
-    addQuizFortopic: (state, action) => {}
+    addQuizIdForTopic: (state, action) => {
+      const { quizId, topicId } = action.payload;
+      state.topics[topicId].quizIds.push(quizId);
+    }
   }
 });
 
-export const selectTopics = (state) => state.topics.topics;
+export const selectTopics = (state) => state;
 
-export const { addTopic } = topicsSlice.actions;
+export const { addTopic, addQuizIdForTopic } = topicsSlice.actions;
 
 export default topicsSlice.reducer;
